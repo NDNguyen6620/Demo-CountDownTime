076c54aef6d616d463f15c56788333d694affc90
NDNguyen6620 <nd.nguyen6620@gmail.com>
2023-06-26 19:31:57 +0700

    Add project files.

---
+++ DemoOc/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿var time = 0.2 * 60;
+
+var itemCountdown = document.querySelector('.countdown-item');
+
+var func = setInterval(function Cd() {
+
+    time--;
+    overtime = time - 0;
+
+    minute = Math.floor(time / 60);
+    second = time % 60;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("second").innerHTML = second;
+    if (overtime < 0) {
+        clearInterval(func);
+        document.getElementById("minute").innerHTML = 0;
+        document.getElementById("second").innerHTML = 0;
+        document.getElementById("timeout").innerHTML = "timeout";
+    }
+}, 1000);
+function resume() {
+    setInterval(function Cd() {
+
+        time--;
+        overtime = time - 0;
+
+        minute = Math.floor(time / 60);
+        second = time % 60;
+        document.getElementById("minute").innerHTML = minute;
+        document.getElementById("second").innerHTML = second;
+        if (overtime < 0) {
+            clearInterval(func);
+            document.getElementById("minute").innerHTML = 0;
+            document.getElementById("second").innerHTML = 0;
+            document.getElementById("timeout").innerHTML = "timeout";
+        }
+    }, 1000);
+}
+function pause() {
+    clearInterval(func);
+}
